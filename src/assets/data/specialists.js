f257f44c7d06909d51fc7dad1f7c26decd0c7072
Yuri <ugajurij123@gmail.com>
2025-12-08 05:06:22 +0500

    fix image paths

--- src/assets/data/specialists.js
+++ src/assets/data/specialists.js
@@ -1,10 +1,10 @@
 export const specialists = [
-  { id: 1, key: 0, image: "/public/images/Electric.png" },
-  { id: 2, key: 1, image: "/public/images/Plumber.png" },
-  { id: 3, key: 2, image: "/public/images/Cleaner.png" },
-  { id: 4, key: 3, image: "/public/images/TechRepair.png" },
-  { id: 5, key: 4, image: "/public/images/Furniture.png" },
-  { id: 6, key: 5, image: "/public/images/Mover.png" },
-  { id: 7, key: 6, image: "/public/images/Handyman.png" },
-  { id: 8, key: 7, image: "/public/images/Tutor.png" },
+  { id: 1, key: 0, image: "/images/Electric.png" },
+  { id: 2, key: 1, image: "/images/Plumber.png" },
+  { id: 3, key: 2, image: "/images/Cleaner.png" },
+  { id: 4, key: 3, image: "/images/TechRepair.png" },
+  { id: 5, key: 4, image: "/images/Furniture.png" },
+  { id: 6, key: 5, image: "/images/Mover.png" },
+  { id: 7, key: 6, image: "/images/Handyman.png" },
+  { id: 8, key: 7, image: "/images/Tutor.png" },
 ];
